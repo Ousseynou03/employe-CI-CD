8facc7c21afe6b2a647649b6a09e5ceb15b462f6
Dione Ousseynou <dioneousseynou03@gmail.com>
2024-02-03 21:41:24 +0100

    comment getId employe

--- src/main/java/com/nedioit/employe/controller/EmployeController.java
+++ src/main/java/com/nedioit/employe/controller/EmployeController.java
@@ -29,10 +29,11 @@ public class EmployeController {
         return iEmployeService.add(employeRequest);
     }
 
-    @GetMapping("/{id}")
-    public EmployeResponse getEmployeById(@PathVariable("id") Long id){
-        return iEmployeService.getEmployeById(id);
-    }
+   // @GetMapping("/{id}")
+  //  public EmployeResponse getEmployeById(@PathVariable("id") Long id){
+  //      return iEmployeService.getEmployeById(id);
+  //  }
+    
 
     @GetMapping("/")
     public List<EmployeResponse> getAllEmployes() {
